8bc65d07e930d54d1e5d63d35d8164e27da766c1
Bryan <lcl25k10@gmail.com>
2023-08-18 11:55:25 +0800

    use npm node erc20Abi

--- main.ts
+++ main.ts
@@ -7,11 +7,12 @@ const signingKey = process.env.SIGNING_KEY || ""; // private key
 const rpcUrl = process.env.RPC_URL || "";
 
 // 首先批准（approve）一個特定的地址以轉移一定數量的 erc20 代幣，然後轉移（transfer）該數量的代幣到該地址
-async function approveAndSendToToken(
+async function approveAndSendToken(
     to:string,
     token:string,
     value:string
 ):Promise<any[]>{
+    // const ERC20_ABI = require("erc-20-abi");
     const ERC20_ABI = require("./erc20Abi.json");
     const provider = new ethers.providers.JsonRpcProvider(rpcUrl); // 連接到乙太網路節點
     const erc20 = new ethers.Contract(token,ERC20_ABI,provider); // 建立 erc20 合約實例
@@ -40,22 +41,33 @@ async function main(){
     const builder = await Presets.Builder.Kernel.init(signer, rpcUrl);
     const address = builder.getSender(); // 利用 builder 建立的 contract address
     console.log(`Account address: ${address}`);
+    console.log('-------------------------------------------------------------');
 
     // create calldata
     const to = address;
     const token = "0x3870419Ba2BBf0127060bCB37f69A1b1C090992B";
     const value = "0";
-    const calls = await approveAndSendToToken(to,token,value);
+    const calls = await approveAndSendToken(to,token,value);
     builder.executeBatch(calls);
-    console.log(builder.getOp());
 
-    // send userOps to bundler
+    // // GasLimit 設高一點
+    // const desiredVerificationGasLimit = ethers.BigNumber.from('0x534bbb');  
+    // builder.setVerificationGasLimit(desiredVerificationGasLimit);
+
+    console.log(builder.getOp());
+    console.log('-------------------------------------------------------------');
+
+    // // send userOps to bundler
     const client = await Client.init(rpcUrl);
-    const res = await client.sendUserOperation(builder, {
-        onBuild: (op) => console.log("Signed UserOperation:", op),
+    const res = await client.sendUserOperation(builder.executeBatch(calls), {
+      onBuild: (op) => console.log("Signed UserOperation:", op),
     });
+    console.log('-------------------------------------------------------------');
+    
     console.log(`UserOpHash: ${res.userOpHash}`);
+    console.log('-------------------------------------------------------------');
     console.log("Waiting for transaction...");
+    console.log('-------------------------------------------------------------');
     const ev = await res.wait();
     console.log(`Transaction hash: ${ev?.transactionHash ?? null}`);
 }
